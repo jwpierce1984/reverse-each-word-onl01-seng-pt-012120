b6ddfae5cafb741f2efbf0478125729878e17ad6
Justin Pierce <jwpierce1984@gmail.com>
2020-01-22 19:03:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
  def reverse_each_word(sentence)
-   sentence.reverse.collect {|word| word.reverse.join(" ")
+   sentence.reverse.collect {|word| word.reverse}.join(" ")
  end
  
 #  def reverse_each_word(sentence)
